ac04dd167858cc90e58f4f17a108c7e332589371
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-09-12 22:19:58 -0300

    landing page almost done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC , #43E7AD , #E1D55D )',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     }
   },
